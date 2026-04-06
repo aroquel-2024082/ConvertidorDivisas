f482cec7b333e653f9dfb9f1acdd750d8a6a1348
aroquel-2024082 <aroquel-2024082@kinal.edu.gt>
2026-04-06 11:46:33 -0600

    Ejercicio en clase pie de pagina

---
+++ FrontConvertidor/src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import './footer.css'
+
+export const Footer = () => {
+    return (
+        <footer className='footer'>
+            <div className='footer-content'>
+                <p>Angel Emmanuel Fiorentin Roquel Acuta</p>
+                <p>Carnet: 2024082</p>
+            </div>
+        </footer>
+    )
+}
